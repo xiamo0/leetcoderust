6a1d0943329951f8d736ae8bac03d67ba22a3e04
xiamo0 <65656443+xiamo0@users.noreply.github.com>
2024-11-16 12:05:27 -0300

    leetcode

--- src/coinchange_322.rs
+++ src/coinchange_322.rs
@@ -1,5 +1,8 @@
 use std::vec;
 
+/**
+ * https://leetcode.cn/problems/coin-change/description/?envType=problem-list-v2&envId=2cktkvj
+ */
 pub fn test() {
     let coins: Vec<i32> = vec![1, 2, 3, 4];
     let amount = 23;
@@ -7,19 +10,18 @@ pub fn test() {
 }
 
 pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
-
     if amount == 0 { return 0; }
     if amount < 0 { return -1; }
     if coins.is_empty() { return -1; }
-    let mut dp: Vec<i32> = vec![0;(amount+1) as usize ];
+    let mut dp: Vec<i32> = vec![0; (amount + 1) as usize];
     let i1 = coins.len();
     for i in 1..amount + 1 {
         dp[i as usize] = amount + 1;
         for j in 0..i1 {
             let x = *(coins.get(j).unwrap());
-            if i - x >= 0  {
+            if i - x >= 0 {
                 let temp = dp[(i - x) as usize] + 1;
-                if  dp[i as usize] > temp {
+                if dp[i as usize] > temp {
                     dp[i as usize] = temp;
                 }
             }
@@ -27,5 +29,4 @@ pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
     }
     let min = if dp[amount as usize] != (amount + 1) { dp[amount as usize] } else { -1 };
     return min;
-
 }
